30c297134a8550500efbc184f1f3fcddbf251fe8
Jang Jaehue <jaehue@jang.io>
2017-08-13 21:12:55 +0800

    add behaviorlog package

---
+++ behaviorlog/behaviorlog.go
@@ -0,0 +1,200 @@
+package behaviorlog
+
+import (
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pangpanglabs/goutils/kafka"
+	"github.com/sirupsen/logrus"
+)
+
+type LogContext struct {
+	Producer *kafka.Producer
+	logger   *logrus.Logger
+
+	Service       string                 `json:"service,omitempty"`
+	Timestamp     time.Time              `json:"timestamp,omitempty"`
+	RequestID     string                 `json:"request_id,omitempty"`
+	RemoteIP      string                 `json:"remote_ip,omitempty"`
+	Host          string                 `json:"host,omitempty"`
+	Uri           string                 `json:"uri,omitempty"`
+	Method        string                 `json:"method,omitempty"`
+	Path          string                 `json:"path,omitempty"`
+	Referer       string                 `json:"referer,omitempty"`
+	UserAgent     string                 `json:"user_agent,omitempty"`
+	Status        int                    `json:"status,omitempty"`
+	Latency       time.Duration          `json:"latency,omitempty"`
+	RequestLength int64                  `json:"request_length,omitempty"`
+	BytesSent     int64                  `json:"bytes_sent,omitempty"`
+	Params        interface{}            `json:"params,omitempty"`
+	Controller    string                 `json:"controller,omitempty"`
+	Action        string                 `json:"action,omitempty"`
+	Body          string                 `json:"body,omitempty"`
+	BizAttr       map[string]interface{} `json:"bizAttr,omitempty"`
+}
+
+const (
+	HeaderXRequestID    = "X-Request-ID"
+	HeaderXForwardedFor = "X-Forwarded-For"
+	HeaderXRealIP       = "X-Real-IP"
+	HeaderContentLength = "Content-Length"
+
+	LogContextName = "behaviorlog"
+)
+
+var logger = logrus.New()
+
+func init() {
+	logger.Formatter = &logrus.JSONFormatter{}
+}
+
+func New(serviceName string, req *http.Request, options ...func(*LogContext)) *LogContext {
+	realIP := req.RemoteAddr
+	if ip := req.Header.Get(HeaderXForwardedFor); ip != "" {
+		realIP = strings.Split(ip, ", ")[0]
+	} else if ip := req.Header.Get(HeaderXRealIP); ip != "" {
+		realIP = ip
+	} else {
+		realIP, _, _ = net.SplitHostPort(realIP)
+	}
+
+	path := req.URL.Path
+	if path == "" {
+		path = "/"
+	}
+
+	requestLength, _ := strconv.ParseInt(req.Header.Get(HeaderContentLength), 10, 64)
+
+	params := map[string]string{}
+	for k, v := range req.URL.Query() {
+		params[k] = v[0]
+	}
+
+	c := &LogContext{
+		// Producer: producer,
+		logger: logger,
+
+		Service:       serviceName,
+		Timestamp:     time.Now(),
+		RequestID:     req.Header.Get(HeaderXRequestID),
+		RemoteIP:      realIP,
+		Host:          req.Host,
+		Uri:           req.RequestURI,
+		Method:        req.Method,
+		Path:          path,
+		Params:        params,
+		Referer:       req.Referer(),
+		UserAgent:     req.UserAgent(),
+		RequestLength: requestLength,
+		// Controller: controller,
+		// Action:     action,
+		BizAttr: map[string]interface{}{},
+	}
+
+	for _, o := range options {
+		if o != nil {
+			o(c)
+		}
+	}
+
+	return c
+}
+
+func KafkaProducer(p *kafka.Producer) func(*LogContext) {
+	return func(l *LogContext) {
+		l.Producer = p
+	}
+
+}
+func (c *LogContext) Clone() *LogContext {
+	return &LogContext{
+		Producer:      c.Producer,
+		logger:        c.logger,
+		Service:       c.Service,
+		Timestamp:     time.Now(),
+		RequestID:     c.RequestID,
+		RemoteIP:      c.RemoteIP,
+		Host:          c.Host,
+		Uri:           c.Uri,
+		Method:        c.Method,
+		Path:          c.Path,
+		Referer:       c.Referer,
+		UserAgent:     c.UserAgent,
+		Status:        c.Status,
+		Latency:       c.Latency,
+		RequestLength: c.RequestLength,
+		BytesSent:     c.BytesSent,
+		Params:        c.Params,
+		Controller:    c.Controller,
+		Action:        c.Action,
+		Body:          c.Body,
+		BizAttr:       map[string]interface{}{},
+	}
+}
+func (c *LogContext) WithBizAttr(key string, value interface{}) *LogContext {
+	c.BizAttr[key] = value
+	return c
+}
+func (c *LogContext) WithControllerAndAction(controller, action string) *LogContext {
+	c.Controller = controller
+	c.Action = action
+	return c
+}
+func (c *LogContext) WithBizAttrs(attrs map[string]interface{}) *LogContext {
+	for k, v := range attrs {
+		c.BizAttr[k] = v
+	}
+	return c
+}
+func (c *LogContext) WithCallURLInfo(method, uri string, param interface{}, responseStatus int) *LogContext {
+	c.Method = method
+	c.Uri = uri
+	c.Params = param
+	c.Status = responseStatus
+	if url, err := url.Parse(uri); err == nil {
+		c.Path = url.Path
+		c.Host = url.Host
+	}
+
+	return c
+}
+func (c *LogContext) Log(action string) {
+	c.Action = action
+	c.Write()
+}
+func (c *LogContext) Write() {
+	c.Latency = time.Now().Sub(c.Timestamp)
+	if c.Producer != nil {
+		c.Producer.Send(c)
+	}
+	c.logger.WithFields(logrus.Fields{
+		"service":        c.Service,
+		"timestamp":      c.Timestamp.UTC().Format(time.RFC3339),
+		"request_id":     c.RequestID,
+		"remote_ip":      c.RemoteIP,
+		"host":           c.Host,
+		"uri":            c.Uri,
+		"method":         c.Method,
+		"path":           c.Path,
+		"referer":        c.Referer,
+		"user_agent":     c.UserAgent,
+		"status":         c.Status,
+		"latency":        c.Latency,
+		"request_length": c.RequestLength,
+		"bytes_sent":     c.BytesSent,
+		"params":         c.Params,
+		"controller":     c.Controller,
+		"action":         c.Action,
+		"body":           c.Body,
+		"bizAttr":        c.BizAttr,
+	}).Info()
+}
+func NewNopContext() *LogContext {
+	return &LogContext{
+		BizAttr: map[string]interface{}{},
+	}
+}
--- echomiddleware/behavior_logger.go
+++ echomiddleware/behavior_logger.go
@@ -2,43 +2,17 @@ package echomiddleware
 
 import (
 	"context"
-	"net/url"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/Shopify/sarama"
 	"github.com/labstack/echo"
+	"github.com/pangpanglabs/goutils/behaviorlog"
 	"github.com/pangpanglabs/goutils/kafka"
 	"github.com/sirupsen/logrus"
 )
 
 const BehaviorLoggerName = "BehaviorLogger"
-
-type BehaviorLogContext struct {
-	producer *kafka.Producer
-	logger   *logrus.Logger
-
-	Service       string                 `json:"service,omitempty"`
-	Timestamp     string                 `json:"timestamp,omitempty"`
-	RequestID     string                 `json:"request_id,omitempty"`
-	RemoteIP      string                 `json:"remote_ip,omitempty"`
-	Host          string                 `json:"host,omitempty"`
-	Uri           string                 `json:"uri,omitempty"`
-	Method        string                 `json:"method,omitempty"`
-	Path          string                 `json:"path,omitempty"`
-	Referer       string                 `json:"referer,omitempty"`
-	UserAgent     string                 `json:"user_agent,omitempty"`
-	Status        int                    `json:"status,omitempty"`
-	Latency       int64                  `json:"latency,omitempty"`
-	RequestLength int64                  `json:"request_length,omitempty"`
-	BytesSent     int64                  `json:"bytes_sent,omitempty"`
-	Params        interface{}            `json:"params,omitempty"`
-	Controller    string                 `json:"controller,omitempty"`
-	Action        string                 `json:"action,omitempty"`
-	Body          string                 `json:"body,omitempty"`
-	BizAttr       map[string]interface{} `json:"bizAttr,omitempty"`
-}
 
 func BehaviorLogger(serviceName string, config KafkaConfig) echo.MiddlewareFunc {
 	var producer *kafka.Producer
@@ -53,53 +27,26 @@ func BehaviorLogger(serviceName string, config KafkaConfig) echo.MiddlewareFunc 
 		producer = p
 	}
 
-	logger := logrus.New()
-	logger.Formatter = &logrus.JSONFormatter{}
-
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) (err error) {
 			req := c.Request()
-			res := c.Response()
-			requestId := req.Header.Get(echo.HeaderXRequestID)
-			if requestId == "" {
-				requestId = res.Header().Get(echo.HeaderXRequestID)
-			}
-			controller, action := getControllerAndAction(c)
-			path := req.URL.Path
-			if path == "" {
-				path = "/"
-			}
+			// fmt.Println("req.RequestURI:", req.RequestURI)
+			// fmt.Println("req.Host:", req.Host)
+			behaviorLogger := behaviorlog.New(serviceName, req, behaviorlog.KafkaProducer(producer))
 
-			behaviorLogger := &BehaviorLogContext{
-				producer: producer,
-				logger:   logger,
-
-				Service:    serviceName,
-				RequestID:  requestId,
-				RemoteIP:   c.RealIP(),
-				Host:       req.Host,
-				Uri:        req.RequestURI,
-				Method:     req.Method,
-				Path:       path,
-				Referer:    req.Referer(),
-				UserAgent:  req.UserAgent(),
-				Controller: controller,
-				Action:     action,
-				BizAttr:    map[string]interface{}{},
-			}
-
-			c.SetRequest(req.WithContext(context.WithValue(req.Context(),
-				"request_id", requestId,
-			)))
 			c.SetRequest(req.WithContext(context.WithValue(req.Context(),
 				BehaviorLoggerName, behaviorLogger,
 			)))
 
-			start := time.Now()
 			if err = next(c); err != nil {
 				c.Error(err)
 			}
-			stop := time.Now()
+
+			res := c.Response()
+
+			behaviorLogger.Status = res.Status
+			behaviorLogger.BytesSent = res.Size
+			behaviorLogger.Controller, behaviorLogger.Action = getControllerAndAction(c)
 
 			params := map[string]interface{}{}
 			for k, v := range c.QueryParams() {
@@ -108,82 +55,14 @@ func BehaviorLogger(serviceName string, config KafkaConfig) echo.MiddlewareFunc 
 			for _, name := range c.ParamNames() {
 				params[name] = c.Param(name)
 			}
-
-			behaviorLogger.Timestamp = start.UTC().Format(time.RFC3339)
-			behaviorLogger.Status = res.Status
-			behaviorLogger.Latency = int64(stop.Sub(start))
-			behaviorLogger.RequestLength, _ = strconv.ParseInt(req.Header.Get(echo.HeaderContentLength), 10, 64)
-			behaviorLogger.BytesSent = res.Size
 			behaviorLogger.Params = params
-			behaviorLogger.Controller = controller
-			behaviorLogger.Action = action
 
-			behaviorLogger.write()
-
+			behaviorLogger.Write()
 			return
 		}
 	}
 }
 
-func NewNopLogger() *BehaviorLogContext {
-	return &BehaviorLogContext{
-		BizAttr: map[string]interface{}{},
-	}
-}
-
-func (c *BehaviorLogContext) WithBizAttr(key string, value interface{}) *BehaviorLogContext {
-	c.BizAttr[key] = value
-	return c
-}
-func (c *BehaviorLogContext) WithBizAttrs(attrs map[string]interface{}) *BehaviorLogContext {
-	for k, v := range attrs {
-		c.BizAttr[k] = v
-	}
-	return c
-}
-func (c *BehaviorLogContext) WithCallURLInfo(method, uri string, param interface{}, responseStatus int) *BehaviorLogContext {
-	c.Method = method
-	c.Uri = uri
-	c.Params = param
-	c.Status = responseStatus
-	if url, err := url.Parse(uri); err == nil {
-		c.Path = url.Path
-		c.Host = url.Host
-	}
-
-	return c
-}
-func (c *BehaviorLogContext) Log(action string) {
-	c.Timestamp = time.Now().UTC().Format(time.RFC3339)
-	c.Action = action
-	c.write()
-}
-func (c *BehaviorLogContext) write() {
-	if c.producer != nil {
-		c.producer.Send(c)
-	}
-	c.logger.WithFields(logrus.Fields{
-		"service":        c.Service,
-		"timestamp":      c.Timestamp,
-		"request_id":     c.RequestID,
-		"remote_ip":      c.RemoteIP,
-		"host":           c.Host,
-		"uri":            c.Uri,
-		"method":         c.Method,
-		"path":           c.Path,
-		"referer":        c.Referer,
-		"user_agent":     c.UserAgent,
-		"status":         c.Status,
-		"latency":        c.Latency,
-		"request_length": c.RequestLength,
-		"bytes_sent":     c.BytesSent,
-		"params":         c.Params,
-		"controller":     c.Controller,
-		"action":         c.Action,
-		"body":           c.Body,
-		"bizAttr":        c.BizAttr,
-	}).Info()
-}
 func getControllerAndAction(c echo.Context) (controller, action string) {
 	if v := c.Get("controller"); v != nil {
 		if controllerName, ok := v.(string); ok {
