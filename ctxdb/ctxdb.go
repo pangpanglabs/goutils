e0da6d14ee04c235b5ef03757fca2a0af1230515
Jang Jaehue <jaehue@jang.io>
2018-12-29 14:52:23 +0800

    #49 Add ctxdb package. Use it in echomiddleware/context_db.go

---
+++ ctxdb/ctxdb.go
@@ -0,0 +1,38 @@
+package ctxdb
+
+import (
+	"context"
+
+	"github.com/go-xorm/xorm"
+	"github.com/pangpanglabs/goutils/kafka"
+)
+
+type ContextDB struct {
+	*xorm.Engine
+}
+
+func New(db *xorm.Engine, service string, config kafka.Config) *ContextDB {
+	db.ShowExecTime()
+	if len(config.Brokers) != 0 {
+		if producer, err := kafka.NewProducer(config.Brokers, config.Topic,
+			kafka.WithDefault(),
+			kafka.WithTLS(config.SSL)); err == nil {
+			db.SetLogger(&dbLogger{serviceName: service, Producer: producer})
+			db.ShowSQL()
+		}
+	}
+
+	return &ContextDB{Engine: db}
+}
+
+func (db *ContextDB) NewSession(ctx context.Context) *xorm.Session {
+	session := db.Engine.NewSession()
+
+	func(session interface{}, ctx context.Context) {
+		if s, ok := session.(interface{ SetContext(context.Context) }); ok {
+			s.SetContext(ctx)
+		}
+	}(session, ctx)
+
+	return session
+}
--- echomiddleware/context_db.go
+++ echomiddleware/context_db.go
@@ -4,43 +4,27 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/pangpanglabs/goutils/behaviorlog"
-
-	"github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
 	"github.com/labstack/echo"
+	"github.com/pangpanglabs/goutils/ctxdb"
 	"github.com/pangpanglabs/goutils/kafka"
 )
 
 const ContextDBName = "DB"
 
-func ContextDB(service string, db *xorm.Engine, kafkaConfig kafka.Config) echo.MiddlewareFunc {
-	db.ShowExecTime()
-	if len(kafkaConfig.Brokers) != 0 {
-		if producer, err := kafka.NewProducer(kafkaConfig.Brokers, kafkaConfig.Topic,
-			kafka.WithDefault(),
-			kafka.WithTLS(kafkaConfig.SSL)); err == nil {
-			db.SetLogger(&dbLogger{serviceName: service, Producer: producer})
-			db.ShowSQL()
-		}
-	}
+func ContextDB(service string, xormEngine *xorm.Engine, kafkaConfig kafka.Config) echo.MiddlewareFunc {
+	db := ctxdb.New(xormEngine, service, kafkaConfig)
 
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			req := c.Request()
+			ctx := req.Context()
 
-			session := db.NewSession()
+			session := db.NewSession(ctx)
 			defer session.Close()
 
-			func(session interface{}, ctx context.Context) {
-				if s, ok := session.(interface{ SetContext(context.Context) }); ok {
-					s.SetContext(ctx)
-				}
-			}(session, req.Context())
-
-			c.SetRequest(req.WithContext(context.WithValue(req.Context(), ContextDBName, session)))
+			c.SetRequest(req.WithContext(context.WithValue(ctx, ContextDBName, session)))
 
 			switch req.Method {
 			case "POST", "PUT", "DELETE", "PATCH":
@@ -66,61 +50,3 @@ func ContextDB(service string, db *xorm.Engine, kafkaConfig kafka.Config) echo.M
 		}
 	}
 }
-
-type SqlLog struct {
-	Service   string      `json:"service,omitempty"`
-	RequestID string      `json:"requestId,omitempty"`
-	ActionID  string      `json:"actionId,omitempty"`
-	Sql       interface{} `json:"sql,omitempty"`
-	Args      interface{} `json:"args,omitempty"`
-	Took      interface{} `json:"took,omitempty"`
-	Timestamp time.Time   `json:"timestamp,omitempty"`
-}
-type dbLogger struct {
-	serviceName string
-	*kafka.Producer
-}
-
-func (logger *dbLogger) Write(v []interface{}) {
-	if len(v) == 0 {
-		return
-	}
-	log := SqlLog{
-		Service:   logger.serviceName,
-		Sql:       v[0],
-		Timestamp: time.Now(),
-	}
-	if ctx, ok := v[len(v)-1].(context.Context); ok {
-		if logContext := behaviorlog.FromCtx(ctx); logContext != nil {
-			log.ActionID = logContext.ActionID
-			log.RequestID = logContext.RequestID
-		}
-		v = v[:len(v)-1]
-	}
-
-	if len(v) == 3 {
-		log.Args = v[1]
-		log.Took = v[2]
-	} else if len(v) == 2 {
-		log.Took = v[1]
-	}
-
-	if d, ok := log.Took.(time.Duration); ok {
-		log.Timestamp = log.Timestamp.Add(-d)
-	}
-
-	logger.Send(&log)
-}
-func (logger *dbLogger) Infof(format string, v ...interface{})  { logger.Write(v) }
-func (logger *dbLogger) Errorf(format string, v ...interface{}) {}
-func (logger *dbLogger) Debugf(format string, v ...interface{}) {}
-func (logger *dbLogger) Warnf(format string, v ...interface{})  {}
-
-func (logger *dbLogger) Debug(v ...interface{})   {}
-func (logger *dbLogger) Error(v ...interface{})   {}
-func (logger *dbLogger) Info(v ...interface{})    {}
-func (logger *dbLogger) Warn(v ...interface{})    {}
-func (logger *dbLogger) SetLevel(l core.LogLevel) {}
-func (logger *dbLogger) ShowSQL(show ...bool)     {}
-func (logger *dbLogger) Level() core.LogLevel     { return 0 }
-func (logger *dbLogger) IsShowSQL() bool          { return true }
