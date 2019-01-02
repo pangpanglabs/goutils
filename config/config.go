9705a06ea5b89a2774e4e5963c9722ef1db85fda
Jang Jaehue <jaehue@jang.io>
2019-01-02 12:47:17 +0800

    #56 Allow set config path ("config" pkg)

--- config/config.go
+++ config/config.go
@@ -3,20 +3,27 @@ package config
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/viper"
 )
 
+var configPath = "."
+
+func SetConfigPath(in string) {
+	configPath = in
+	viper.AddConfigPath(in)
+}
 func Read(env string, config interface{}) error {
 	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
+	viper.AddConfigPath(configPath)
 
 	if err := viper.ReadInConfig(); err != nil {
 		return fmt.Errorf("Fatal error config file: %s \n", err)
 	}
 
 	if env != "" {
-		f, err := os.Open("config." + env + ".yml")
+		f, err := os.Open(filepath.Join(configPath, "config."+env+".yml"))
 		if err != nil {
 			return fmt.Errorf("Fatal error config file: %s \n", err)
 		}
