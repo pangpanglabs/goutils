4fedd82dfe8535264a4b8162c8179f2e6b5b42cd
Jang Jaehue <jaehue@jang.io>
2019-04-01 13:31:04 +0800

    #69 Add hostname to behaviorlog

--- negronimiddleware/behavior_logger.go
+++ negronimiddleware/behavior_logger.go
@@ -3,6 +3,7 @@ package negronimiddleware
 import (
 	"context"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/Shopify/sarama"
@@ -12,22 +13,31 @@ import (
 )
 
 type BehaviorLogger struct {
+	hostname    string
 	serviceName string
 	producer    *kafka.Producer
 }
 
 func NewBehaviorLogger(serviceName string, brokers []string, topic string, options ...func(*sarama.Config)) *BehaviorLogger {
-	b := BehaviorLogger{serviceName: serviceName}
+	hostname, err := os.Hostname()
+	if err != nil {
+		logrus.WithError(err).Error("Fail to get hostname")
+	}
+
+	b := BehaviorLogger{serviceName: serviceName, hostname: hostname}
 	options = append(options, option)
 	if p, err := kafka.NewProducer(brokers, topic, options...); err != nil {
 		logrus.Error("Create Kafka Producer Error", err)
 	} else {
 		b.producer = p
 	}
+
 	return &b
 }
 func (b *BehaviorLogger) ServeHTTP(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
 	behaviorLogger := behaviorlog.New(b.serviceName, req, behaviorlog.KafkaProducer(b.producer))
+	behaviorLogger.Hostname = b.hostname
+
 	next(rw, req.WithContext(context.WithValue(req.Context(),
 		behaviorlog.LogContextName, behaviorLogger,
 	)))
